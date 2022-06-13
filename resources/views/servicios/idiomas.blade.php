5e8f486bf6bf9a6a32eb471b54aa171cda7e43f4
Enrique Rodriguez <enrique.rv0704@gmail.com>
2022-06-13 13:55:58 -0500

    upd

--- resources/views/servicios/idiomas.blade.php
+++ resources/views/servicios/idiomas.blade.php
@@ -19,19 +19,16 @@
       <div class="row">
         <div class="col-md-12">
           <p class="mt-4">
-            El lenguaje es la base de nuestra comunicación y por lo tanto, la parte medular de las relaciones humanas. A través del lenguaje somos capaces de expresar nuestros pensamientos y emociones. La actual integración internacional ha generado una creciente dependencia entre los distintos países del mundo que ha dado como resultado la unificación de diversas culturas, sociedades y mercados. Todo esto ha generado una serie de transformaciones sociales, políticas y económicas que han hecho inminente la necesidad de dominar otras lenguas.
+            El lenguaje es la base de nuestra comunicación y, por lo tanto, la parte medular de las relaciones humanas. A través del lenguaje somos capaces de expresar nuestros pensamientos y emociones. La actual integración internacional ha generado una creciente dependencia entre los distintos países del mundo que ha dado como resultado la unificación de diversas culturas, sociedades y mercados. Todo esto ha generado una serie de transformaciones sociales, políticas y económicas que han hecho inminente la necesidad de dominar otras lenguas.
           </p>
           <p class="mt-4">
             Centro de Idiomas te proporciona las herramientas necesarias para que construyas tu futuro.
           </p>
           <p class="mt-4">
-            Estudia cualquiera de los 5 idiomas que impartimos:
+            Estudia cualquiera de los idiomas que impartimos de forma presencial
             <ul>
               <li>Inglés</li>
-              <li>Alemán</li>
-              <li>Italiano</li>
               <li>Francés</li>
-              <li>Chino Mandarín</li>
             </ul>
           </p>
           <p class="mt-4">
@@ -48,28 +45,51 @@
               <li>Grupos Reducidos</li>
               <li>Examen de ubicación sin costo.</li>
               <li>Cursos para todas las edades (niños, jóvenes y adultos).</li>
-              <li>Diploma avalado por la Universidad y la Certificación SEP-CENNI.</li>
             </ul>
           </p>
+
+          <p class="mt-4">
+            Como opción adicional, gracias a la vinculación que tenemos con Oxford TTC, ahora también está disponible el curso de cualquier idioma en línea Alphable; el cual está conformado por clases personalizadas a cualquier hora y día de la semana.
+          </p>
+
+          <p class="mt-4">
+            Otra excelente ventaja de este acuerdo es que, con nosotros al ser un centro certificador, podrás obtener la certificación internacional del idioma inglés Oxford de acuerdo a tu nivel. Así mismo, tienes la opción de adquirir la certificación nacional CENNI con el resultado obtenido en tu certificación Oxford por un costo adicional.
+          </p>
+
+          <p class="mt-4">
+            Como apoyo para tu preparación puedes hacer uso de la plataforma GEAR, una plataforma digital de ejercicios para reforzar tu nivel de inglés e incrementarlo.
+          </p>
+
+          <p class="mt-4 h3 mb-4">
+            ¡No te pierdas la oportunidad de crecer como estudiante y profesional en el centro de idiomas Haller!
+          </p>
+
+
         </div>
 
         <div class="col-md-2 mt-2 text-center">
           <img src="{{ env('APP_URL') }}/assets/images2022/servicios/esfera1.png" width="85%">
+          Inglés
         </div>
         <div class="col-md-2 mt-2 text-center">
           <img src="{{ env('APP_URL') }}/assets/images2022/servicios/esfera2.png" width="85%">
+          Francés
         </div>
         <div class="col-md-2 mt-2 text-center">
           <img src="{{ env('APP_URL') }}/assets/images2022/servicios/esfera3.png" width="85%">
+          Italiano
         </div>
         <div class="col-md-2 mt-2 text-center">
           <img src="{{ env('APP_URL') }}/assets/images2022/servicios/esfera4.png" width="85%">
+          Alemán
         </div>
         <div class="col-md-2 mt-2 text-center">
           <img src="{{ env('APP_URL') }}/assets/images2022/servicios/esfera5.png" width="85%">
+          Portugués
         </div>
         <div class="col-md-2 mt-2 text-center">
           <img src="{{ env('APP_URL') }}/assets/images2022/servicios/esfera6.png" width="85%">
+          Chino
         </div>
 
     </section>
